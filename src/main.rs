f6bd158d8665b148ecc35bade7cb3ffce11d5a9e
Daniel <daniel.cordeiro2@fatec.sp.gov.br>
2026-01-30 20:29:24 -0300

    ✨ feat: Adiciona suporte a [DELETE]

--- src/main.rs
+++ src/main.rs
@@ -76,6 +76,41 @@ Connection: close\r\n\
 
 }
 
+fn http_delete(host: &str, path: &str, port: u16) -> Result<String, Box<dyn std::error::Error>> {
+    let mut stream = TcpStream::connect(format!("{}:{}", host, port))?;
+
+    let request = format!(
+        "DELETE {} HTTP/1.1\r\n\
+Host: {}\r\n\
+Connection: close\r\n\
+\r\n\
+",
+        path, host
+    );
+
+    stream.write_all(request.as_bytes())?;
+
+    let mut reader = BufReader::new(stream);
+
+    let mut status = String::new();
+    reader.read_line(&mut status)?;
+    println!("{}", status);
+
+    loop {
+        let mut headers = String::new();
+        reader.read_line(&mut headers)?;
+
+        if headers == "\r\n" { break; };
+
+        println!("{}", headers);
+    }
+
+    let mut body = String::new();
+    reader.read_to_string(&mut body)?;
+    Ok(body)
+
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
@@ -93,6 +128,10 @@ fn main() {
             let body = http_get(&args[2], &args[3], args[4].parse().unwrap()).unwrap();
             println!("{}", body);
         },
+        "DELETE" => {
+            let body = http_delete(&args[2], &args[3], args[4].parse().unwrap()).unwrap();
+            println!("{}", body);
+        }
         _ => panic!("Metodo invalido")
     }
 }
