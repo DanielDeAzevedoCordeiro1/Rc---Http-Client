81021355e59b5501ac8911facde24b3f5eb36037
Daniel <daniel.cordeiro2@fatec.sp.gov.br>
2026-02-06 18:09:10 -0300

    ♻️  refactor: Funcao main

--- src/main.rs
+++ src/main.rs
@@ -1,232 +1,29 @@
-use std::io::{BufRead, BufReader, Read, Write};
-use std::net::TcpStream;
+use crate::client::send_request;
+use crate::http::{HttpRequest, Method, ServerAddress};
 
-pub struct Request<'a> {
-    method: &'a str,
-    host: &'a str,
-    path: &'a str,
-    port: u16,
-    body: Option<&'a str>,
-}
-
-impl<'a> Request<'a> {
-    pub fn  new(method: &'a str, host: &'a str, path: &'a str, port: u16, body: Option<&'a str>) -> Self {
-        Self { method , host, path, port, body }
-    }
-}
-
-
-fn http_get(request: Request) -> Result<String, Box<dyn std::error::Error>> {
-
-    let mut stream = TcpStream::connect(format!("{}:{}", request.host, request.port))?;
-
-    let request = format!(
-        "{} {} HTTP/1.1\r\n\
-Host: {}\r\n\
-Connection: close\r\n\
-\r\n",
-        request.method, request.path, request.host
-    );
-
-    stream.write_all(request.as_bytes())?;
-
-    let mut reader = BufReader::new(stream);
-
-    let mut status = String::new();
-    reader.read_line(&mut status)?;
-
-    loop {
-        let mut headers = String::new();
-        reader.read_line(&mut headers)?;
-
-        if headers == "\r\n" { break; };
-    }
-
-    let mut body = String::new();
-    reader.read_to_string(&mut body)?;
-
-    Ok(body)
-}
-
-
-fn http_post(request: Request) -> Result<String, Box<dyn std::error::Error>> {
-    let mut stream = TcpStream::connect(format!("{}:{}", request.host, request.port))?;
-    let content_length = request.body.unwrap().len();
-
-    let request = format!(
-        "{} {} HTTP/1.1\r\n\
-Host: {}\r\n\
-Content-Type: application/json\r\n\
-Content-Length: {}\r\n\
-Connection: close\r\n\
-\r\n\
-{}",
-        request.method, request.path, request.host, content_length, request.body.unwrap()
-    );
-
-    stream.write_all(request.as_bytes())?;
-
-    let mut reader = BufReader::new(stream);
-
-    let mut status = String::new();
-    reader.read_line(&mut status)?;
-
-    loop {
-        let mut headers = String::new();
-        reader.read_line(&mut headers)?;
-
-        if headers == "\r\n" { break; };
-    }
-
-    let mut body = String::new();
-    reader.read_to_string(&mut body)?;
-    Ok(body)
-
-}
-
-fn http_delete(request: Request) -> Result<String, Box<dyn std::error::Error>> {
-    let mut stream = TcpStream::connect(format!("{}:{}", request.host, request.port))?;
-
-    let request = format!(
-        "{} {} HTTP/1.1\r\n\
-Host: {}\r\n\
-Connection: close\r\n\
-\r\n\
-",
-        request.method, request.path, request.host
-    );
-
-    stream.write_all(request.as_bytes())?;
-
-    let mut reader = BufReader::new(stream);
-
-    let mut status = String::new();
-    reader.read_line(&mut status)?;
-
-    loop {
-        let mut headers = String::new();
-        reader.read_line(&mut headers)?;
-
-        if headers == "\r\n" { break; };
-    }
-
-    let mut body = String::new();
-    reader.read_to_string(&mut body)?;
-    Ok(body)
-
-}
-
-fn http_put(request: Request) -> Result<String, Box<dyn std::error::Error>> {
-    let mut stream = TcpStream::connect(format!("{}:{}", request.host, request.port))?;
-    let content_length = request.body.unwrap().len();
-
-    let request = format!(
-        "{} {} HTTP/1.1\r\n\
-Host: {}\r\n\
-Content-Type: application/json\r\n\
-Content-Length: {}\r\n\
-Connection: close\r\n\
-\r\n\
-{}",
-        request.method, request.path, request.host, content_length, request.body.unwrap()
-    );
-
-    stream.write_all(request.as_bytes())?;
-
-    let mut reader = BufReader::new(stream);
-
-    let mut status = String::new();
-    reader.read_line(&mut status)?;
-
-    loop {
-        let mut headers = String::new();
-        reader.read_line(&mut headers)?;
-
-        if headers == "\r\n" { break; };
-    }
-
-    let mut body = String::new();
-    reader.read_to_string(&mut body)?;
-    Ok(body)
-
-}
-
-fn http_patch(request: Request) -> Result<String, Box<dyn std::error::Error>> {
-    let mut stream = TcpStream::connect(format!("{}:{}", request.host, request.port))?;
-    let content_length = request.body.unwrap().len();
-
-    let request = format!(
-        "{} {} HTTP/1.1\r\n\
-Host: {}\r\n\
-Content-Type: application/json\r\n\
-Content-Length: {}\r\n\
-Connection: close\r\n\
-\r\n\
-{}",
-        request.method, request.path, request.host, content_length, request.body.unwrap()
-    );
-
-    stream.write_all(request.as_bytes())?;
-
-    let mut reader = BufReader::new(stream);
-
-    let mut status = String::new();
-    reader.read_line(&mut status)?;
-
-    loop {
-        let mut headers = String::new();
-        reader.read_line(&mut headers)?;
-
-        if headers == "\r\n" { break; };
-    }
-
-    let mut body = String::new();
-    reader.read_to_string(&mut body)?;
-    Ok(body)
-}
+mod client;
+mod http;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-
-    if args.len() > 6{
-        panic!("Digite os argumentos na seguinte ordem ---> rc METODO HOST PATH PORTA BODY");
+    if args.len() < 5 || args.len() > 6 {
+        panic!("Uso: rc METODO HOST PATH PORTA [BODY]");
     }
 
     let body = if args.len() == 6 {
-        Some(&args[5] as &str)
+        Some(args[5].clone())
     } else {
         None
     };
 
-    let request_args = Request::new(
-        &args[1],
-        &args[2],
-        &args[3],
-        args[4].parse::<u16>().unwrap(),
-        body
-    );
+    let method = Method::from(args[1].as_str());
 
-    match request_args.method.to_uppercase().as_str() {
-        "POST" => {
-            let body = http_post(request_args).unwrap();
-            println!("{}", body);
-        },
-        "GET" => {
-            let body = http_get(request_args).unwrap();
-            println!("{}", body);
-        },
-        "PATCH" => {
-            let body = http_patch(request_args).unwrap();
-            println!("{}", body);
-        }
-        "PUT" => {
-            let body = http_put(request_args).unwrap();
-            println!("{}", body);
-        },
-        "DELETE" => {
-            let body = http_delete(request_args).unwrap();
-            println!("{}", body);
-        }
-        _ => panic!("Metodo invalido")
+    let server_info = ServerAddress::from([args[2].as_str(), args[4].as_str()]);
+
+    let request = HttpRequest::new(method, args[3].clone(), body);
+
+    match send_request(request, server_info) {
+        Ok(resp_body) => println!("{}", resp_body),
+        Err(e) => eprintln!("Erro: {}", e),
     }
 }
